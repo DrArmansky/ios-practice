c5d1ff93b9e041324f2a6cc2961ce03099c1d390
Kirill Sagan <sagan.k89@gmail.com>
2019-11-04 17:07:17 +0300

    add queue generic

---
+++ queueGeneric.playground/Contents.swift
@@ -0,0 +1,51 @@
+
+protocol Queue {
+    associatedtype Element: Equatable
+    
+    var list: [Element] { get set }
+    
+    mutating func enqueue(_ element: Element)
+    
+    mutating func dequeue() -> Element?
+    
+    subscript(i: Int) -> Element { get }
+}
+
+struct SomeCollection<T: Equatable>: Queue {
+    
+    internal var list = [T]()
+
+    var isEmpty: Bool {
+        return list.isEmpty
+    }
+    
+    subscript(i: Int) -> T {
+        return list[i]
+    }
+    
+    
+    public mutating func enqueue(_ element: T) {
+        list.append(element)
+    }
+
+    public mutating func dequeue() -> T? {
+        guard !list.isEmpty, list.first != nil else { return nil }
+        
+        return list.removeFirst()
+    }
+
+    public func peek() -> T? {
+        return list.first
+    }
+}
+
+var someOrder: SomeCollection<Int> = SomeCollection()
+
+someOrder.enqueue(5)
+someOrder.enqueue(1)
+someOrder.enqueue(2)
+someOrder.dequeue()
+
+print(someOrder)
+
+

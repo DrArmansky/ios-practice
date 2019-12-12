a2541b9ed26aacdf132e2cd397bc4597365b482a
Kirill Sagan <sagan.k89@gmail.com>
2019-12-13 00:31:15 +0300

    add cell for collection view

---
+++ CollectionView/CollectionView/ImageCell.swift
@@ -0,0 +1,22 @@
+//
+//  ImageCell.swift
+//  CollectionView
+//
+//  Created by Kira on 10.12.2019.
+//  Copyright © 2019 Kira. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+
+    @IBOutlet weak var image: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    func setup(imageName: String) {
+        image.contentMode = .scaleAspectFit
+    }
+}
---
+++ CollectionView/CollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Kira on 10.12.2019.
+//  Copyright © 2019 Kira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /*let paths = Bundle.main.paths(forResourcesOfType: "jpg", inDirectory: "Assets/Images")
+        print()*/
+    }
+
+}
+
+
+

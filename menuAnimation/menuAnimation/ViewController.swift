e415937675412a07b52c93d044bc9a31ded06d9d
Kirill Sagan <sagan.k89@gmail.com>
2019-12-15 20:47:32 +0300

    create mmenu animation poject

---
+++ menuAnimation/menuAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  menuAnimation
+//
+//  Created by Kira on 15.12.2019.
+//  Copyright © 2019 Kira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

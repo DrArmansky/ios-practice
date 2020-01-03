5ff9669d712e7ebd6808f509bd365970e41d3d52
Kirill Sagan <sagan.k89@gmail.com>
2020-01-04 01:34:21 +0300

    add open menu animation

--- menuAnimation/menuAnimation/ViewController.swift
+++ menuAnimation/menuAnimation/ViewController.swift
@@ -9,23 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var menu: UIView!
+    
+    private let menuWidthMultiplicator = 0.7
+    private let menuleftInnerConstraint = 40
+    
+    private let menu = { () -> UIView in
+        let menu = UIView()
+        menu.layer.shadowColor = UIColor.black.cgColor
+        menu.layer.shadowOpacity = 0.4
+        menu.layer.shadowOffset = .zero
+        menu.layer.shadowRadius = 10
+        menu.layer.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        return menu
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func viewDidLayoutSubviews() {
-        menu.layer.shadowColor = UIColor.black.cgColor
-        menu.layer.shadowOpacity = 0.4
-        menu.layer.shadowOffset = .zero
-        menu.layer.shadowRadius = 10
-        
-        // попробовать удалить констрейн
-        NSLayoutConstraint(item: menu, attribute: NSLayoutConstraint.Attribute.left, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.centerX, multiplier: 1, constant: -menu.frame.width).isActive = true
+        setupMenu()
     }
-
-
+    
+    private func setupMenu() {
+        let menuWidth = self.view.frame.width * CGFloat(menuWidthMultiplicator)
+        let menuHeight = self.view.frame.height
+        
+        menu.frame = CGRect(x: -menuWidth, y: .zero, width: menuWidth, height: menuHeight)
+        self.view.addSubview(menu)
+    }
+    
+    private func showMenuAnumation() {
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0,
+            options: [.curveEaseOut],
+            animations: {
+                self.menu.layer.position = CGPoint(
+                    x: self.menu.layer.position.x + self.menu.frame.width,
+                    y: self.menu.layer.position.y
+                )
+            },
+            completion: { isComplited in
+                guard isComplited else { return }
+                
+                UIView.animate(
+                    withDuration: 0.3,
+                    delay: 0,
+                    options: [.curveEaseOut],
+                    animations: {
+                        self.menu.layer.position = CGPoint(
+                            x: self.menu.layer.position.x - CGFloat(self.menuleftInnerConstraint),
+                            y: self.menu.layer.position.y
+                        )
+                    }
+                )
+            }
+        )
+    }
+    
+    @IBAction func openMenu(_ sender: Any) {
+        showMenuAnumation()
+    }
 }
 
